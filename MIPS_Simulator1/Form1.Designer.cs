6ec7e449cef0c31fc0478ba539ceae2fe41f8cee
FehmiYuce <113864075+FehmiYuce@users.noreply.github.com>
2024-05-13 19:32:27 +0300

    MIPS

--- MIPS_Simulator1/Form1.Designer.cs
+++ MIPS_Simulator1/Form1.Designer.cs
@@ -20,220 +20,232 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			textBox1 = new TextBox();
-			button1 = new Button();
-			dataGridView1 = new DataGridView();
-			Column1 = new DataGridViewTextBoxColumn();
-			Column2 = new DataGridViewTextBoxColumn();
-			Column3 = new DataGridViewTextBoxColumn();
-			label1 = new Label();
-			dataGridView2 = new DataGridView();
-			Column4 = new DataGridViewTextBoxColumn();
-			Column5 = new DataGridViewTextBoxColumn();
-			Column6 = new DataGridViewTextBoxColumn();
-			label2 = new Label();
-			label3 = new Label();
-			dataGridView3 = new DataGridView();
-			Column7 = new DataGridViewTextBoxColumn();
-			Column8 = new DataGridViewTextBoxColumn();
-			Column9 = new DataGridViewTextBoxColumn();
-			Column10 = new DataGridViewTextBoxColumn();
-			Column11 = new DataGridViewTextBoxColumn();
-			button2 = new Button();
-			((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
-			((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
-			((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
-			SuspendLayout();
-			// 
-			// textBox1
-			// 
-			textBox1.Location = new Point(27, 52);
-			textBox1.Multiline = true;
-			textBox1.Name = "textBox1";
-			textBox1.Size = new Size(560, 219);
-			textBox1.TabIndex = 0;
-			textBox1.TextChanged += textBox1_TextChanged;
-			// 
-			// button1
-			// 
-			button1.Location = new Point(27, 23);
-			button1.Name = "button1";
-			button1.Size = new Size(75, 23);
-			button1.TabIndex = 1;
-			button1.Text = "Run";
-			button1.UseVisualStyleBackColor = true;
-			button1.Click += button1_Click;
-			// 
-			// dataGridView1
-			// 
-			dataGridView1.BackgroundColor = Color.White;
-			dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
-			dataGridView1.GridColor = Color.Black;
-			dataGridView1.Location = new Point(593, 49);
-			dataGridView1.Name = "dataGridView1";
-			dataGridView1.Size = new Size(333, 219);
-			dataGridView1.TabIndex = 2;
-			dataGridView1.CellContentClick += dataGridView1_CellContentClick;
-			// 
-			// Column1
-			// 
-			Column1.HeaderText = "Name";
-			Column1.Name = "Column1";
-			// 
-			// Column2
-			// 
-			Column2.HeaderText = "Number";
-			Column2.Name = "Column2";
-			// 
-			// Column3
-			// 
-			Column3.HeaderText = "Value";
-			Column3.Name = "Column3";
-			// 
-			// label1
-			// 
-			label1.AutoSize = true;
-			label1.Location = new Point(593, 31);
-			label1.Name = "label1";
-			label1.Size = new Size(54, 15);
-			label1.TabIndex = 3;
-			label1.Text = "Registers";
-			label1.Click += label1_Click;
-			// 
-			// dataGridView2
-			// 
-			dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			dataGridView2.Columns.AddRange(new DataGridViewColumn[] { Column4, Column5, Column6 });
-			dataGridView2.Location = new Point(27, 304);
-			dataGridView2.Name = "dataGridView2";
-			dataGridView2.Size = new Size(899, 143);
-			dataGridView2.TabIndex = 4;
-			dataGridView2.CellContentClick += dataGridView2_CellContentClick;
-			// 
-			// Column4
-			// 
-			Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column4.HeaderText = "Address";
-			Column4.Name = "Column4";
-			// 
-			// Column5
-			// 
-			Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column5.HeaderText = "Code";
-			Column5.Name = "Column5";
-			// 
-			// Column6
-			// 
-			Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column6.HeaderText = "Source";
-			Column6.Name = "Column6";
-			// 
-			// label2
-			// 
-			label2.AutoSize = true;
-			label2.Location = new Point(27, 286);
-			label2.Name = "label2";
-			label2.Size = new Size(108, 15);
-			label2.TabIndex = 5;
-			label2.Text = "Instrucion Memory";
-			// 
-			// label3
-			// 
-			label3.AutoSize = true;
-			label3.Location = new Point(30, 452);
-			label3.Name = "label3";
-			label3.Size = new Size(79, 15);
-			label3.TabIndex = 6;
-			label3.Text = "Data Memory";
-			label3.Click += label3_Click;
-			// 
-			// dataGridView3
-			// 
-			dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-			dataGridView3.Columns.AddRange(new DataGridViewColumn[] { Column7, Column8, Column9, Column10, Column11 });
-			dataGridView3.Location = new Point(27, 470);
-			dataGridView3.Name = "dataGridView3";
-			dataGridView3.Size = new Size(899, 133);
-			dataGridView3.TabIndex = 7;
-			dataGridView3.CellContentClick += dataGridView3_CellContentClick;
-			// 
-			// Column7
-			// 
-			Column7.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column7.HeaderText = "Address";
-			Column7.Name = "Column7";
-			// 
-			// Column8
-			// 
-			Column8.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column8.HeaderText = "Value(+0)";
-			Column8.Name = "Column8";
-			// 
-			// Column9
-			// 
-			Column9.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column9.HeaderText = "Value(+4)";
-			Column9.Name = "Column9";
-			// 
-			// Column10
-			// 
-			Column10.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column10.HeaderText = "Value(+8)";
-			Column10.Name = "Column10";
-			// 
-			// Column11
-			// 
-			Column11.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
-			Column11.HeaderText = "Value(+c)";
-			Column11.Name = "Column11";
-			// 
-			// button2
-			// 
-			button2.Location = new Point(141, 278);
-			button2.Name = "button2";
-			button2.Size = new Size(75, 23);
-			button2.TabIndex = 8;
-			button2.Text = "IM";
-			button2.UseVisualStyleBackColor = true;
-			button2.Click += button2_Click;
-			// 
-			// Form1
-			// 
-			AutoScaleDimensions = new SizeF(7F, 15F);
-			AutoScaleMode = AutoScaleMode.Font;
-			ClientSize = new Size(938, 615);
-			Controls.Add(button2);
-			Controls.Add(dataGridView3);
-			Controls.Add(label3);
-			Controls.Add(label2);
-			Controls.Add(dataGridView2);
-			Controls.Add(label1);
-			Controls.Add(dataGridView1);
-			Controls.Add(button1);
-			Controls.Add(textBox1);
-			ImeMode = ImeMode.On;
-			Name = "Form1";
-			Text = "Form1";
-			Load += Form1_Load;
-			((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
-			((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
-			((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
-			ResumeLayout(false);
-			PerformLayout();
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            label1 = new Label();
+            dataGridView2 = new DataGridView();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewTextBoxColumn();
+            label2 = new Label();
+            label3 = new Label();
+            dataGridView3 = new DataGridView();
+            Column7 = new DataGridViewTextBoxColumn();
+            Column8 = new DataGridViewTextBoxColumn();
+            Column9 = new DataGridViewTextBoxColumn();
+            Column10 = new DataGridViewTextBoxColumn();
+            Column11 = new DataGridViewTextBoxColumn();
+            button2 = new Button();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(27, 52);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(560, 219);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(27, 23);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Run";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3 });
+            dataGridView1.GridColor = Color.Black;
+            dataGridView1.Location = new Point(593, 49);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(333, 219);
+            dataGridView1.TabIndex = 2;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
+            // 
+            // Column1
+            // 
+            Column1.HeaderText = "Name";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.HeaderText = "Number";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.HeaderText = "Value";
+            Column3.Name = "Column3";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(593, 31);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Registers";
+            label1.Click += label1_Click;
+            // 
+            // dataGridView2
+            // 
+            dataGridView2.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView2.Columns.AddRange(new DataGridViewColumn[] { Column4, Column5, Column6 });
+            dataGridView2.Location = new Point(27, 304);
+            dataGridView2.Name = "dataGridView2";
+            dataGridView2.Size = new Size(899, 143);
+            dataGridView2.TabIndex = 4;
+            dataGridView2.CellContentClick += dataGridView2_CellContentClick;
+            // 
+            // Column4
+            // 
+            Column4.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column4.HeaderText = "Address";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column5.HeaderText = "Code";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column6.HeaderText = "Source";
+            Column6.Name = "Column6";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(27, 286);
+            label2.Name = "label2";
+            label2.Size = new Size(108, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Instrucion Memory";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(27, 460);
+            label3.Name = "label3";
+            label3.Size = new Size(79, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Data Memory";
+            label3.Click += label3_Click;
+            // 
+            // dataGridView3
+            // 
+            dataGridView3.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView3.Columns.AddRange(new DataGridViewColumn[] { Column7, Column8, Column9, Column10, Column11 });
+            dataGridView3.Location = new Point(27, 478);
+            dataGridView3.Name = "dataGridView3";
+            dataGridView3.Size = new Size(899, 133);
+            dataGridView3.TabIndex = 7;
+            dataGridView3.CellContentClick += dataGridView3_CellContentClick;
+            // 
+            // Column7
+            // 
+            Column7.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column7.HeaderText = "Address";
+            Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            Column8.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column8.HeaderText = "Value(+0)";
+            Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            Column9.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column9.HeaderText = "Value(+4)";
+            Column9.Name = "Column9";
+            // 
+            // Column10
+            // 
+            Column10.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column10.HeaderText = "Value(+8)";
+            Column10.Name = "Column10";
+            // 
+            // Column11
+            // 
+            Column11.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column11.HeaderText = "Value(+c)";
+            Column11.Name = "Column11";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(141, 278);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 8;
+            button2.Text = "IM";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(141, 452);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 9;
+            button3.Text = "DM";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(938, 623);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(dataGridView3);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(dataGridView2);
+            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            ImeMode = ImeMode.On;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
+        #endregion
 
-		private TextBox textBox1;
+        private TextBox textBox1;
 		private Button button1;
 		private DataGridView dataGridView1;
 		private DataGridViewTextBoxColumn Column1;
@@ -253,5 +265,6 @@
 		private DataGridViewTextBoxColumn Column10;
 		private DataGridViewTextBoxColumn Column11;
 		private Button button2;
-	}
+        private Button button3;
+    }
 }
--- MIPS_Simulator1/Form1.cs
+++ MIPS_Simulator1/Form1.cs
@@ -2,151 +2,191 @@ using Microsoft.Win32;
 
 namespace MIPS_Simulator1
 {
-	public partial class Form1 : Form
-	{
-		private Dictionary<string, string> registers = Registers.RegisterMap;
-		public Form1()
-		{
-			InitializeComponent();
-		}
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, string> registers = Registers.RegisterMap;
+        public Form1()
+        {
+            InitializeComponent();
+        }
 
-		private void button1_Click(object sender, EventArgs e)//run
-		{
-			// TextBox'tan assembly kodunu oku
-			string assemblyCode = textBox1.Text;
+        private void button1_Click(object sender, EventArgs e)//run
+        {
+            // TextBox'tan assembly kodunu oku
+            string assemblyCode = textBox1.Text;
 
-			// Derleme iþlemini gerçekleþtir
-			string[] assemblyCodeArray = assemblyCode.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
-			CompileAssembly(assemblyCodeArray);
-		}
+            // Derleme iþlemini gerçekleþtir
+            string[] assemblyCodeArray = assemblyCode.Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None);
+            CompileAssembly(assemblyCodeArray);
+        }
 
 
-		private void textBox1_TextChanged(object sender, EventArgs e)
-		{
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
-		{
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
-		}
+        }
 
-		private void label1_Click(object sender, EventArgs e)
-		{
+        private void label1_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void label3_Click(object sender, EventArgs e)
-		{
+        private void label3_Click(object sender, EventArgs e)
+        {
 
-		}
+        }
 
-		private void Form1_Load(object sender, EventArgs e)
-		{
-			SaveRegistersToTable();
-			//UpdateInstructionMemoryTable(new string[0], new List<string>()); bunu bir düþün
-		}
-		private void SaveRegistersToTable()//register table
-		{
-			// Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
-			dataGridView1.Rows.Clear();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SaveRegistersToTable();
+            UpdateInstructionMemoryTable(Array.Empty<string>(), new List<string>());
+            
+        }
+        private void SaveRegistersToTable()//register table
+        {
+            // Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
+            dataGridView1.Rows.Clear();
 
-			// Registerlarý tabloya ekler
-			foreach (var register in registers)
-			{
-				string name = register.Key;
-				string number = name.Substring(1); // "r" veya "s" karakterini kaldýrýr
-				string value = "0*00000000"; // Baþlangýç deðerleri isteðe baðlý olarak atanabilir
+            // Registerlarý tabloya ekler
+            foreach (var register in registers)
+            {
+                string name = register.Key;
+                string number = name.Substring(1); // "r" veya "s" karakterini kaldýrýr
+                string value = "0*00000000"; // Baþlangýç deðerleri isteðe baðlý olarak atanabilir
 
-				// Yeni bir satýr oluþtur ve kayýtlarý tabloya ekle
-				DataGridViewRow row = new DataGridViewRow();
-				row.CreateCells(dataGridView1);
-				row.Cells[0].Value = name;
-				row.Cells[1].Value = number;
-				row.Cells[2].Value = value;
-				dataGridView1.Rows.Add(row);
-			}
-		}
-		private void CompileAssembly(string[] assemblyCode)// textbox
-		{
-			try
-			{
-				List<string> machineCode = new List<string>();
+                // Yeni bir satýr oluþtur ve kayýtlarý tabloya ekle
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+                row.Cells[0].Value = name;
+                row.Cells[1].Value = number;
+                row.Cells[2].Value = value;
+                dataGridView1.Rows.Add(row);
+            }
+        }
+        private void CompileAssembly(string[] assemblyCode)// textbox
+        {
+            try
+            {
+                List<string> machineCode = new List<string>();
 
-				foreach (string instruction in assemblyCode)
-				{
-					string compiledInstruction = Compiler.CompileInstruction(instruction);
-					string hexCode = Convert.ToInt32(compiledInstruction, 2).ToString("X").PadLeft(8, '0');
-					machineCode.Add(hexCode);
-				}
+                foreach (string instruction in assemblyCode)
+                {
+                    string compiledInstruction = Compiler.CompileInstruction(instruction);
+                    string hexCode = Convert.ToInt32(compiledInstruction, 2).ToString("X").PadLeft(8, '0');
+                    machineCode.Add(hexCode);
+                }
 
-				// Derlenmiþ makine kodunu göster
-				DisplayMachineCode(machineCode);
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show("Hata oluþtu: " + ex.Message);
-			}
-		}
+                // Derlenmiþ makine kodunu göster
+                DisplayMachineCode(machineCode);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluþtu: " + ex.Message);
+            }
+        }
 
-		private void DisplayMachineCode(List<string> machineCode)// textbox
-		{
-			// Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
-			dataGridView1.Rows.Clear();
+        private void DisplayMachineCode(List<string> machineCode)// textbox
+        {
+            // Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
+            dataGridView1.Rows.Clear();
 
-			// Derlenmiþ makine kodunu tabloya ekle
-			foreach (string code in machineCode)
-			{
-				// Yeni bir satýr oluþtur ve makine kodunu tabloya ekle
-				DataGridViewRow row = new DataGridViewRow();
-				row.CreateCells(dataGridView1);
-				row.Cells[0].Value = code;
-				dataGridView1.Rows.Add(row);
-			}
-		}
+            // Derlenmiþ makine kodunu tabloya ekle
+            foreach (string code in machineCode)
+            {
+                // Yeni bir satýr oluþtur ve makine kodunu tabloya ekle
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+                row.Cells[0].Value = code;
+                dataGridView1.Rows.Add(row);
+            }
+        }
 
-		private void button2_Click(object sender, EventArgs e)//IM
-		{
-			// TextBox'tan assembly kodunu oku
-			string[] assemblyCode = textBox1.Lines;
+        private void button2_Click(object sender, EventArgs e)//IM
+        {
+            // TextBox'tan assembly kodunu oku
+            string[] assemblyCode = textBox1.Lines;
 
-			// Assembly kodunu derle
-			List<string> machineCode = Compiler.CompileToHex(new List<string>(assemblyCode));
+            // Varsayýlan makine kodu oluþtur
+            List<string> defaultMachineCode = new List<string>();
+            for (int i = 0; i < assemblyCode.Length; i++)
+            {
+                defaultMachineCode.Add("0x00000000");
+            }
 
-			// Instruction Memory tablosunu güncelle
-			UpdateInstructionMemoryTable(assemblyCode, machineCode);
-		}
+            // Instruction Memory tablosunu güncelle (varsayýlan deðerlerle)
+            UpdateInstructionMemoryTable(assemblyCode, defaultMachineCode);
+        }
 
-		private void UpdateInstructionMemoryTable(string[] assemblyCode, List<string> machineCode)
-		{
-			// Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
-			dataGridView2.Rows.Clear();
+        private void UpdateInstructionMemoryTable(string[] assemblyCode, List<string> machineCode)
+        {
+            // Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
+            dataGridView2.Rows.Clear();
 
-			// Assembly kodunu tabloya ekle
-			for (int i = 0; i < assemblyCode.Length; i++)
-			{
-				string instruction = assemblyCode[i];
-				string address = i.ToString(); // Adres
-				string code = machineCode[i]; // Makine kodu
-				string source = "Assembly"; // Kaynak
+            // Assembly kodunu ve makine kodunu tabloya ekle
+            for (int i = 0; i < assemblyCode.Length; i++)
+            {
+                string instruction = assemblyCode[i];
+                string address = "0x" + (i * 4).ToString("X8"); // Adres
+                string code = machineCode[i]; // Makine kodu
+                string source = instruction; // Kaynak
 
-				DataGridViewRow row = new DataGridViewRow();
-				row.CreateCells(dataGridView2);
-				row.Cells[0].Value = address;
-				row.Cells[1].Value = code;
-				row.Cells[2].Value = source;
-				dataGridView2.Rows.Add(row);
-			}
-		}
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView2);
+                row.Cells[0].Value = address;
+                row.Cells[1].Value = code;
+                row.Cells[2].Value = source;
+                dataGridView2.Rows.Add(row);
+            }
+        }
 
-		private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
-		{
 
-		}
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
 
-		private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
-		{
+        }
 
-		}
-	}
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        private void UpdateDataMemoryTable(int[] dataMemory)
+        {
+            // Temizleme iþlemi, eðer tabloda varsa mevcut kayýtlarý kaldýrýr
+            dataGridView3.Rows.Clear();
+
+            // Adres ve deðer baþlýklarýný ekle
+            dataGridView3.Columns.Add("Address", "Address");
+            for (int i = 0; i < 8; i++)
+            {
+                dataGridView3.Columns.Add("Value+" + (i * 4).ToString("X2"), "Value+" + (i * 4).ToString("X2"));
+            }
+
+            // Data Memory tablosunu güncelle
+            for (int i = 0; i < dataMemory.Length; i += 8)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView3);
+                row.Cells[0].Value = "0x" + (i * 4).ToString("X8"); // Adresi ekle
+                for (int j = 0; j < 8; j++)
+                {
+                    row.Cells[j + 1].Value = "0x" + dataMemory[i + j].ToString("X8"); // Deðerleri ekle
+                }
+                dataGridView3.Rows.Add(row);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // TextBox'tan veri belleði içeriðini oku
+            string[] dataMemory = textBox1.Lines;
+
+            // Veri belleðini güncelle
+            //UpdateDataMemoryTable(dataMemory);
+        }
+    }
 }
